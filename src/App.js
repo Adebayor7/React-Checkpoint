75693d948f3c5d10e82936acb06e79b9065f028f
Adebayor <idrisadeebayorr12@gmail.com>
2021-11-14 21:16:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form class="userform">
+          <label for ="Username">First Name: </label>
+          <input type ="text" id="fname" name= "fname" ></input>
+          <label for ="Username">Last Name: </label>
+          <input type ="text" id="lname" name="lname" ></input>
+          <label for ="pnumber">Phone number: </label>
+          <input type ="text" id="pnumber" name="pnumber"></input>
+          <label for ="email">Email:</label>
+          <input type ="password" name="user_pass"></input>
+          <label for ="password">Password:</label> 
+          <input type ="text" name="password"></input>
+          <button>Submit</button>
+       </form>
     </div>
   );
 }
